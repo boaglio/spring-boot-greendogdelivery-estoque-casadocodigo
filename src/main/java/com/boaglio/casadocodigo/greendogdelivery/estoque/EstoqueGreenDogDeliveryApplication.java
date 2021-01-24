7fee967f418bc9be177caee292b28a98917fa5f5
boaglio <boaglio@gmail.com>
2021-01-24 15:58:40 -0300

    +Spring Boot Admin client

--- src/main/java/com/boaglio/casadocodigo/greendogdelivery/estoque/EstoqueGreenDogDeliveryApplication.java
+++ src/main/java/com/boaglio/casadocodigo/greendogdelivery/estoque/EstoqueGreenDogDeliveryApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-
-@EnableAdminServer
+ 
 @SpringBootApplication
 @EnableMongoRepositories("com.boaglio") 
 @EnableReactiveMongoRepositories("com.boaglio") 
